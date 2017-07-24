cf812fe420c32608eac885b9d405f1eebd41dd53
Sivesh Sukumar <sivesh@yourvert.co.uk>
2017-07-24 14:50:21 +0100

    update gcp_functions.py

--- gcp_functions.py
+++ gcp_functions.py
@@ -8,64 +8,6 @@ pubsub_client = pubsub.Client().from_service_account_json('./backend/secrets/Sma
 #topic = pubsub_client.topic(topic)
 
 
-
-def list_subscriptions(topic_name):
-    """Lists all subscriptions for a given topic."""
-    pubsub_client = pubsub.Client()
-    topic = pubsub_client.topic(topic_name)
-
-    for subscription in topic.list_subscriptions():
-        print(subscription.name)
-
-def list_topics():
-    """Lists all Pub/Sub topics in the current project."""
-    pubsub_client = pubsub.Client()
-
-    for topic in pubsub_client.list_topics():
-        print(topic.name)
-def list_subscriptions(topic_name):
-    """Lists all subscriptions for a given topic."""
-    pubsub_client = pubsub.Client()
-    topic = pubsub_client.topic(topic_name)
-
-    for subscription in topic.list_subscriptions():
-        print(subscription.name)
-
-def receive_message(topic_name, subscription_name):
-    """Receives a message from a pull subscription."""
-    pubsub_client = pubsub.Client()
-    topic = pubsub_client.topic(topic_name)
-    subscription = topic.subscription(subscription_name)
-
-    # Change return_immediately=False to block until messages are
-    # received.
-    results = subscription.pull(return_immediately=True)
-
-    print('Received {} messages.'.format(len(results)))
-
-    for ack_id, message in results:
-        print('* {}: {}, {}'.format(
-            message.message_id, message.data, message.attributes))
-
-    # Acknowledge received messages. If you do not acknowledge, Pub/Sub will
-    # redeliver the message.
-    if results:
-        subscription.acknowledge([ack_id for ack_id, message in results])
-
-
-def create_subscription(topic_name, subscription_name):
-    """Create a new pull subscription on the given topic."""
-    pubsub_client = pubsub.Client()
-    topic = pubsub_client.topic(topic_name)
-
-    subscription = topic.subscription(subscription_name)
-    subscription.create()
-
-    print('Subscription {} created on topic {}.'.format(
-        subscription.name, topic.name))
-
-
-
 def publish_message(topic_name, data):
     """Publishes a message to a Pub/Sub topic with the given data."""
     pubsub_client = pubsub.Client()
@@ -104,8 +46,5 @@ def receive_message(topic_name, subscription_name):
 
 
 
-
-#list_topics()
-#publish_message('atomicity-messages', 'test')
 
 
