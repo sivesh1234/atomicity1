34b055295013df286f9cd132b2af206f26d5c16e
Sivesh Sukumar <sivesh@yourvert.co.uk>
2017-08-03 10:57:02 +0100

    Update atomicity.py with 50ms timer to reduce CPU usage

--- atomicity.py
+++ atomicity.py
@@ -1,14 +1,15 @@
 import sys
 import threading
+import time
 import atexit
 import pyaudio
 import numpy as np
-from PyQt4 import QtGui, QtCore
 import datetime
+from PyQt4 import QtGui, QtCore
 from gcp_functions import *
-
+#Script found online to gather PyAudio frames
 class MicrophoneRecorder(object):
-    def __init__(self, rate=4000, chunksize=1024):
+    def __init__(self, rate=1000, chunksize=1024):
         self.rate = rate
         self.chunksize = chunksize
         self.p = pyaudio.PyAudio()
@@ -36,8 +37,6 @@ class MicrophoneRecorder(object):
             frames = self.frames
             self.frames = []
             return frames
-       
-
     def start(self):
         self.stream.start_stream()
 
@@ -52,10 +51,12 @@ class LiveSoundAnalysis:
     def __init__(self):
         # init class data
         self.initData()
-        # connect slots
-        self.connectSlots()
+        
         while 1:
             self.handleNewData()
+            time.sleep(0.05)
+        
+
 
     def initData(self):
         mic = MicrophoneRecorder()
@@ -65,13 +66,11 @@ class LiveSoundAnalysis:
         self.mic = mic
         self.mic.start()
 
-        # computes the parameters that will be used during plotting
+        # computes the parameters that will be used
         self.freq_vect = np.fft.rfftfreq(mic.chunksize,
                                          1./mic.rate)
         self.time_vect = np.arange(mic.chunksize, dtype=np.float32) / mic.rate * 1000
 
-    def connectSlots(self):
-        pass
         
     def handleNewData(self):
         """ handles the asynchroneously collected sound chunks """
@@ -84,6 +83,7 @@ class LiveSoundAnalysis:
             else:
                 frames = frames
             current_frame = frames[-1]
+            
             
             # computes the fft signal
             fft_frame = np.fft.rfft(current_frame)
@@ -105,6 +105,7 @@ class LiveSoundAnalysis:
                         file.write('potential siren recorded at %s.\n' % (datetime.datetime.now()))
                     message1 = 'potential siren recorded at %s.\n' % (datetime.datetime.now())
                     publish_message('atomicity-messages',message1)
+                    
             for x in combined:
                 if x[1] == [1.0] and 1400 < x[0] < 1600:
                     print 'siren 3 (police car) detected recorded at %s.\n' % (datetime.datetime.now())
@@ -120,10 +121,10 @@ class LiveSoundAnalysis:
                     message3 = 'siren 2 (fire engine) recorded at %s.\n' % (datetime.datetime.now())
                     publish_message('atomicity-messages',message3)
             
-            # return modal frequency
-            #for x in combined:
-                #if x[1] == [1.0]:
-                   #print x[0]
+            #return modal frequency
+            for x in combined:
+                if x[1] == [1.0]:
+                   print x[0]
 
 
 
